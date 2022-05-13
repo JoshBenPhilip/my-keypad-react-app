65a4ea0c115e51ee6c1b137d4610bd22d9eee6e1
JoshBenPhilip <joshben4c@gmail.com>
2022-05-13 16:37:40 -0400

    added 0 button and cleaned up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [numberString, setNumberString] = useState("")
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <keypadOutput>{numberString}</keypadOutput>
+        <keypadLine1>
+        <button onClick= {()=>setNumberString ( numberString + "1" )}>1</button>
+        <button onClick= {()=>setNumberString (numberString + "2" )}>2</button>
+        <button onClick= {()=>setNumberString (numberString + "3" )}>3</button>
+        </keypadLine1>
+        <keypadLine1>
+        <button onClick= {()=>setNumberString ( numberString + "4" )}>4</button>
+        <button onClick= {()=>setNumberString (numberString + "5" )}>5</button>
+        <button onClick= {()=>setNumberString (numberString + "6" )}>6</button>
+        </keypadLine1>
+        <keypadLine1>
+        <button onClick= {()=>setNumberString ( numberString + "7" )}>7</button>
+        <button onClick= {()=>setNumberString (numberString + "8" )}>8</button>
+        <button onClick= {()=>setNumberString (numberString + "9" )}>9</button>
+        </keypadLine1>
+        
+
+        <keypadLine1>
+        {/* <button onClick= {()=>setNumberString ("")}>clear</button> */}
+        
+        <button onClick= {()=>setNumberString (numberString.slice(0,-1))}>←</button>
+        <button onClick= {()=>setNumberString (numberString + "0" )}>0</button>
+        <button onClick= {()=>setNumberString ("")}>E</button>
+        </keypadLine1>
+        <div></div>&nbsp;
       </header>
     </div>
   );
